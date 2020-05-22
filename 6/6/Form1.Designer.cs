eba975068fee62f723dda7d7d12ac83a5369296f
magemaster555 <35275000+magemaster555@users.noreply.github.com>
2020-05-22 14:43:38 +0300

    Add files via upload

---
+++ 6/6/Form1.Designer.cs
@@ -0,0 +1,65 @@
+﻿namespace _6
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.SystemColors.Info;
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox1.Location = new System.Drawing.Point(12, 12);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(683, 189);
+            this.textBox1.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(707, 213);
+            this.Controls.Add(this.textBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ 6/6/Form1.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace _6
+{
+    public partial class Form1 : Form
+    {
+        List<Point> points = new List<Point>();
+        List<Line> lines = new List<Line>();
+        public Form1()
+        {
+            InitializeComponent();
+            string line;
+
+            System.IO.StreamReader file = new System.IO.StreamReader(@"coords.txt");
+            while ((line = file.ReadLine()) != null)
+            {
+                if (line.Length <= 0) continue;
+                string[] s = line.Split(' ');
+                Point p = new Point(double.Parse(s[0]), double.Parse(s[1]));
+                points.Add(p);
+            }
+            file.Close();
+            for(int i = 0; i < points.Count(); i++)
+            {
+                Point p = points[i];
+                for (int j = 0; j < points.Count(); j++)
+                {
+                    Point pp = points[j];
+                    if (p.X == pp.X && p.Y == pp.Y) continue;
+                    bool flag = false;
+                    Line temp = new Line(p, pp);
+                    for (int l = 0; l < lines.Count(); l++)
+                    { if (temp.Len == lines[l].Len) { flag = true; break; } }
+                    if (!flag)
+                        lines.Add(temp);
+                }
+            }
+            int max_i = 0, min_i = 0;
+            for (int i = 0; i < lines.Count(); i++)
+            {
+                double cur = lines[i].Len;
+                if (cur > lines[max_i].Len) max_i = i;
+                if (cur < lines[min_i].Len) min_i = i;
+            }
+            textBox1.Text += "Самый длинный отрезок:"+Environment.NewLine;
+            textBox1.Text += string.Format("\tДлина: {0}. Точки: ({1}, {2}) .. ({3}, {4})", lines[max_i].Len, lines[max_i].P1.X, lines[max_i].P1.Y, lines[max_i].P2.X, lines[max_i].P2.Y) +Environment.NewLine + Environment.NewLine;
+            textBox1.Text += "Самый короткий отрезок:" + Environment.NewLine;
+            textBox1.Text += string.Format("\tДлина: {0}. Точки: ({1}, {2}) .. ({3}, {4})", lines[min_i].Len, lines[min_i].P1.X, lines[min_i].P1.Y, lines[min_i].P2.X, lines[min_i].P2.Y) +Environment.NewLine;
+        }
+    }
+}
+public struct Point
+{
+    public Point(double x, double y)
+    {
+        X = x;
+        Y = y;
+    }
+    public double X { get; }
+    public double Y { get; }
+}
+public struct Line
+{
+    public Line(Point p1, Point p2)
+    {
+        P1 = p1; P2 = p2;
+        Len = Math.Sqrt(Math.Pow((p2.X - p1.X), 2) + Math.Pow((p2.Y - p1.Y), 2));
+    }
+    public double Len;
+    public Point P1 { get; }
+    public Point P2 { get; }
+}
